68b706a2819bce5717c16e9d35e8c98f9ecd1822
tibinsunny <tibinsunny95@gmail.com>
2020-12-13 10:40:27 -0600

    Status Downloader backup

---
+++ script.js
@@ -0,0 +1,43 @@
+
+function insertAfter(referenceNode, newNode) {
+   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+   
+ }
+ window.onerror = function(message, url, lineNumber) {  
+   return true; // prevents browser error messages  
+};
+setInterval(function(){ 
+
+
+if(document.getElementById("zxczxc")==null)
+ var element=((document.getElementsByClassName("BiQht"))[0])
+    {
+     var test=document.getElementsByClassName('wIUe-')[0]
+
+     if( ((test.getElementsByClassName("_1VzZY"))[0])){
+      var img_url=(((test.getElementsByClassName("_1VzZY"))[0]).src)
+     
+     }
+      else 
+    {
+      var img_url=(((test.getElementsByTagName("video"))[0]).src)
+      
+     }
+     
+
+        var btn = document.createElement("img");
+        btn.setAttribute("id", "zxczxc");
+        btn.setAttribute("src", "https://i.imgur.com/Dmy72eY.png");
+        btn.setAttribute("height", "45px");
+        btn.setAttribute("width", "40px");
+        btn.innerHTML = 'This is a test';
+        var link = document.createElement("a")
+        link.setAttribute("href", img_url);
+        link.setAttribute("download","");
+        link.appendChild(btn)
+         // element.appendChild(btn);
+        insertAfter(element,link)
+       
+        
+    }
+ }, 200);
