3cfb15fe1e6e46b90b9369ea27dcf7ebf3207386
tibinsunny <tibinsunny95@gmail.com>
2021-05-21 17:04:05 +0530

    bug fix #1

--- script.js
+++ script.js
@@ -6,39 +6,34 @@ function insertAfter(referenceNode, newNode) {
  window.onerror = function(message, url, lineNumber) {  
    return true; // prevents browser error messages  
 };
+let img_url=""
 setInterval(function(){ 
+   //Check if the Element is already added or not 
+if(document.getElementById("whatsapp-web-id-created")==null){
+   //get the location to add the Download Icon
+    let element=((document.getElementsByClassName("iEDES"))[0])
+    //check if the status is Video or Image
+     if( ((document.getElementsByClassName("_2M4qA"))[0]))
+     {
 
-
-if(document.getElementById("zxczxc")==null)
- var element=((document.getElementsByClassName("BiQht"))[0])
-    {
-     var test=document.getElementsByClassName('wIUe-')[0]
-
-     if( ((test.getElementsByClassName("_1VzZY"))[0])){
-      var img_url=(((test.getElementsByClassName("_1VzZY"))[0]).src)
-     
-     }
-      else 
-    {
-      var img_url=(((test.getElementsByTagName("video"))[0]).src)
+         img_url=((document.getElementsByClassName("_2M4qA")[0].children)[0]).currentSrc
       
      }
-     
-
-        var btn = document.createElement("img");
-        btn.setAttribute("id", "zxczxc");
+     else 
+   {
+         img_url=(((test.getElementsByTagName("video"))[0]).src) 
+    }
+        let btn = document.createElement("img");
+        btn.setAttribute("id", "whatsapp-web-id-created");
         btn.setAttribute("src", "https://i.imgur.com/Dmy72eY.png");
         btn.setAttribute("height", "45px");
         btn.setAttribute("width", "40px");
         btn.innerHTML = 'This is a test';
-        var link = document.createElement("a")
+        let link = document.createElement("a")
         link.setAttribute("href", img_url);
         link.setAttribute("download","");
-        link.setAttribute("title","⚠ Watch Full Video Before Download to Avoid Errors");
+        link.setAttribute("title","⚠ Watch Full Video Before Downloading to Avoid Errors");
         link.appendChild(btn)
-         // element.appendChild(btn);
-        insertAfter(element,link)
-       
-        
+      insertAfter(element.children[0],link)  
     }
  }, 200);
